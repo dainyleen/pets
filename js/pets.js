6eff39f244886c5e6ab8a9d0a981c4c53d993d21
Dainyleen Masangkay <dainyleen@gmail.com>
2021-06-16 14:20:33 +1200

    Created js folder

---
+++ js/pets.js
@@ -0,0 +1,44 @@
+const pets = [
+  {
+    name: 'Benjie',
+    type: 'Dog',
+    breed: 'Australian Shepherd',
+    age: 5,
+    photo: 'img/aussie.jpg'
+  },
+  {
+    name: 'Luca',
+    type: 'Dog',
+    breed: 'Dachshund',
+    age: 2,
+    photo: 'img/dachshund.jpg'
+  },
+  {
+    name: 'Libby',
+    type: 'Dog',
+    breed: 'Golden Retriever',
+    age: 8,
+    photo: 'img/golden.jpg'
+  },
+  {
+    name: 'Queenie',
+    type: 'Cat',
+    breed: 'Persian',
+    age: 4,
+    photo: 'img/persian.jpg'
+  },
+  {
+    name: 'Tyson',
+    type: 'Dog',
+    breed: 'Pug',
+    age: 1,
+    photo: 'img/pug.jpg'
+  },
+  {
+    name: 'Cookie',
+    type: 'Cat',
+    breed: 'Domestic Shorthair',
+    age: 9,
+    photo: 'img/tabby.jpg'
+  }
+]
