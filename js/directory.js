82cc40f27abb37ec5c9beddde09a3347515e1880
Dainyleen Masangkay <dainyleen@gmail.com>
2021-06-16 14:35:20 +1200

    Pets showing on browser

--- js/directory.js
+++ js/directory.js
@@ -4,7 +4,10 @@ for (let i = 0; i < pets.length; i++) {
   let pet = pets[i]
   html += `
     <h2>${pet.name}</h2>
+    <h3>${pet.type} | ${pet.breed}</h3>
+    <p>${pet.age}</p>
+    <img src="${pet.photo}" alt="${pet.breed}">
   `
 }
 
-console.log(html)
+document.querySelector('main').insertAdjacentHTML('beforeend', html)
